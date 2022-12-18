f7d1ae34101e6052393c341f3fb7f7f76903972e
tatsuya-ogi312 <112700622+tatsuya-ogi312@users.noreply.github.com>
2022-12-18 14:42:58 +0900

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+	java
+	id("org.springframework.boot") version "2.7.6"
+	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+}
+
+group = "in.tech-camp"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:2.3.0")
+	compileOnly("org.projectlombok:lombok")
+	runtimeOnly("com.mysql:mysql-connector-j")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
